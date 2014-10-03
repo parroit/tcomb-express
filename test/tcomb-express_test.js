412d415fa41d6ca1bc9ad37e01e45349a817d606
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-10-03 21:16:11 +0200

    first release

--- test/tcomb-express_test.js
+++ test/tcomb-express_test.js
@@ -13,11 +13,13 @@ chai.expect();
 chai.should();
 
 var request = require('request');
-
+var rimraf = require('rimraf');
 var tcombExpress = require('../lib/tcomb-express.js');
 var webAuth = require('web-auth');
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
 
 tcombExpress.setup(express);
 
@@ -37,44 +39,84 @@ describe('tcombExpress', function() {
     });
 
     before(function(done) {
+        if (fs.existsSync('./test/assets')) {
 
+            rimraf.sync('./test/assets');
+        }
         this.app = express();
 
         this.app.use(bodyParser.json());
 
-        var authRoutes = tcombExpress(webAuth.storage.pouchImplementation);
+
+
+        var w = new webAuth.storage.PouchImplementation('test/assets');
+        var auth = webAuth.storage.interface(w);
+        var _this = this;
+
+
+
+        var authRoutes = tcombExpress(auth);
 
         this.app.use('/test', authRoutes);
-        
+
 
         this.app.use(function(err, req, res, next) {
             res.status(err.status || 500);
-            res.render('error', {
+            res.json({
                 message: err.message,
                 error: err
             });
         });
 
 
-        this.server = this.app.listen(6000, function() {
-            done();
-        });
+
+
+
+        auth.save({
+            username: 'parroit',
+            password: 'secret',
+            email: 'parroit@eban.so',
+            admin: true
+        })
+
+        .then(function(result) {
+            _this.server = _this.app.listen(6000, function() {
+                done();
+            });
+        })
+
+        .catch(done);
 
     });
 
     it('define a route for each function', function(done) {
-        request('http://localhost:6000/test/index', function(error, response, body) {
-            if (error) {
-                return done(new Error(error));
-            }
+        request({
+                uri: 'http://localhost:6000/test/get',
+                method: 'post',
+                body: '["parroit"]',
+                headers: {
+                    'content-type': 'application/json'
+                }
+            },
+            function(error, response, body) {
+                if (error) {
+                    return done(new Error(error));
+                }
 
+                response.statusCode.should.be.equal(200);
 
-            response.statusCode.should.be.equal(200);
+                var user = JSON.parse(response.body);
+                delete user._rev;
+                user.should.be.deep.equal({
+                    _id: 'parroit',
+                    admin: true ,
+                    email: 'parroit@eban.so',
+                    password: 'secret',
+                    username: 'parroit'
+                });
 
-            JSON.parse(response.body).should.be.deep.equal({title:'Express'});
-
-            done();
-        });
+                done();
+            });
     });
 
 });
